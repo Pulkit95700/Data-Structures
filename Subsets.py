f594a804a305e134758d92e40ead29872d2cf11d
Pulkit95700 <94851500+Pulkit95700@users.noreply.github.com>
2021-12-20 21:49:35 +0530

    Add files via upload

---
+++ Subsets.py
@@ -0,0 +1,13 @@
+def Subsets(input_list):
+	if len(input_list) == 1:
+		return [[],[input_list[0]]]
+	else:
+		output = []
+		subset_list = Subsets(input_list[1:])
+		f_element = input_list[0]
+		for ele_list in subset_list:
+			output.append([f_element] + ele_list)
+		output.extend(subset_list)
+		return output
+
+print(Subsets([1,2,3,4]),end = "")
